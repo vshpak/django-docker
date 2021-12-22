709d5339284931e9d304fc14e83daebd2f969e80
Писарчук Елена <ed_pisarchuk@mail.ru>
2021-12-22 16:58:44 +0300

    удалила лишний комментарий

--- store/urls.py
+++ store/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from store.views import PublisherViewSet, JournalViewSet, BookViewSet, ArticleViewSet
 
-router = DefaultRouter()  # trailing_slash=False
+router = DefaultRouter()
 router.register("publishers", PublisherViewSet)
 router.register("journals", JournalViewSet)
 router.register("books", BookViewSet)
