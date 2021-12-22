d21b8a40377d4bc0e5ad6bcd473065bf30891920
Елена Писарчук <e.pisarchuk@qsoft.ru>
2021-12-22 16:52:39 +0300

    правка механизма наполнения

--- helpers/write_articles.py
+++ helpers/write_articles.py
@@ -1,26 +1,33 @@
 import os
-from kaggle.api.kaggle_api_extended import KaggleApi
-import pandas as pd
+
+from kaggle.rest import ApiException
 
 from app.settings import HELPERS_PATH
+from kaggle.api.kaggle_api_extended import KaggleApi
+import pandas as pd
+import logging
+logger = logging.getLogger(__name__)
+
 
 FILE_PATH = os.path.join(HELPERS_PATH, "authorpublications.txt")
 
 
-def get_articles_data():
-    user_data = pd.read_csv(
+def get_author_publications_from_csv():
+    return pd.read_csv(
         FILE_PATH,
         sep="	",
         header=0,
         names=["username", "first_name", "last_name", "articles"],
     ).iloc[:150]
 
+
+def get_publications_data_from_kaggle(author_publications):
     api = KaggleApi()
     api.authenticate()
 
     articles = {}
 
-    for num, item in user_data.iterrows():
+    for num, item in author_publications.iterrows():
         for article_code in item["articles"].split(","):
 
             code = article_code.split("-")[0]
@@ -30,78 +37,59 @@ def get_articles_data():
 
             if code not in articles.keys():
                 articles[code] = {}
-                i = 1
                 try:
-                    while i < 100:
+                    for i in range(1, 100):
                         downloaded_file = api.datasets_download_file(
                             "amitkumarjaiswal",
                             "nlp-publications-on-acl-anthology",
                             f"publication/publication/{code}-{i}.json",
                         )
-                        print(
+                        logger.debug(
                             f"Downloaded file: publication/publication/{code}-{i}.json"
                         )
                         for article in downloaded_file:
                             articles[code][article["id"]] = article
-                        i += 1
-                except Exception:
-                    print(
+                except ApiException:
+                    logger.debug(
                         "End of files in category"
                         if (i > 1)
                         else f"Category data {code}-{i} is empty"
                     )
 
-    return [user_data, articles]
+    return articles
 
 
 def write_data_to_model(apps, schema_editor):
 
-    user_model = apps.get_model("store", "Author")
+    author_model = apps.get_model("store", "Author")
     article_model = apps.get_model("store", "Article")
     journal_model = apps.get_model("store", "Journal")
     publisher_model = apps.get_model("store", "Publisher")
-    user_data, articles = get_articles_data()
-    for category in articles.keys():
-        for code in articles[category].keys():
+    publications_by_authors = get_author_publications_from_csv()
+    articles = get_publications_data_from_kaggle(publications_by_authors)
+    for category in articles.values():
+        for code in category.values():
 
-            try:
-                publisher = publisher_model.objects.get(
-                    name=articles[category][code]["publisher"]
-                )
-            except publisher_model.DoesNotExist:
-                publisher = None
-
-            if publisher is None:
-                publisher = publisher_model.objects.create(
-                    address=articles[category][code]["address"],
-                    name=articles[category][code]["publisher"],
-                )
-
-            try:
-                journal = journal_model.objects.get(
-                    name=articles[category][code]["booktitle"], publisher=publisher.pk
-                )
-            except journal_model.DoesNotExist:
-                journal = None
-
-            if journal is None:
-                journal = journal_model.objects.create(
-                    name=articles[category][code]["booktitle"], publisher=publisher
-                )
-
-            created_article = article_model.objects.create(
-                name=articles[category][code]["title"], journal=journal
+            publisher, created = publisher_model.objects.get_or_create(
+                name=code["publisher"],
+                defaults={"address": code["address"]},
             )
-            articles[category][code]["model"] = created_article
-            articles[category][code]["authors"] = []
+            journal, created = journal_model.objects.get_or_create(
+                name=code["booktitle"],
+                publisher=publisher
+            )
+            created_article = article_model.objects.create(
+                name=code["title"], journal=journal
+            )
+            code["model"] = created_article
+            code["authors"] = []
 
-    for number, user in user_data.iterrows():
-        created_user = user_model.objects.create(
-            username=user["username"],
-            first_name=user["first_name"],
-            last_name=user["last_name"],
+    for number, author_data in publications_by_authors.iterrows():
+        created_user = author_model.objects.create(
+            first_name=author_data["first_name"],
+            last_name=author_data["last_name"],
         )
-        article_codes = user["articles"].split(",")
+        article_codes = author_data["articles"].split(",")
         for code in article_codes:
             category = code.split("-")[0]
             if (
