bf6dab774684caf10d7075e49f830d18759e5bbe
Писарчук Елена <ed_pisarchuk@mail.ru>
2021-12-22 17:00:00 +0300

    улучшение сериализаторов

--- store/serializers.py
+++ store/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from store.models import Publisher, Journal, Book, Article
+from store.models import Publisher, Journal, Book, Article, Author
 
 
 class PublisherSerializer(serializers.ModelSerializer):
@@ -9,19 +9,36 @@ class PublisherSerializer(serializers.ModelSerializer):
         fields = ["id", "name", "address"]
 
 
-class JournalSerializer(serializers.ModelSerializer):
+class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Journal
-        fields = ["id", "name", "article_set", "publisher"]
+        model = Author
+        fields = ["id", "first_name", "last_name"]
 
 
 class ArticleSerializer(serializers.ModelSerializer):
+    authors = serializers.PrimaryKeyRelatedField(many=True, queryset=Author.objects.all())
+    journal = serializers.PrimaryKeyRelatedField(queryset=Journal.objects.all())
+
     class Meta:
         model = Article
         fields = ["id", "name", "journal", "authors"]
 
 
+class JournalSerializer(serializers.ModelSerializer):
+
+    articles = ArticleSerializer(many=True, source='article_set', read_only=True)
+    publisher = serializers.PrimaryKeyRelatedField(queryset=Publisher.objects.all())
+
+    class Meta:
+        model = Journal
+        fields = ["id", "name", "publisher", "articles"]
+
+
 class BookSerializer(serializers.ModelSerializer):
+
+    authors = serializers.PrimaryKeyRelatedField(many=True, queryset=Author.objects.all())
+    publisher = serializers.PrimaryKeyRelatedField(queryset=Publisher.objects.all())
+
     class Meta:
         model = Book
         fields = ["id", "name", "publisher", "authors"]
